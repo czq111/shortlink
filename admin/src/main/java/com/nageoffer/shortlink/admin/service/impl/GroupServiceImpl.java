fc63c40e1b33395362f869948fa96ab985a4a35a
czqq123456 <1807099124@qq.com>
2024-06-08 14:49:04 +0800

    短链接分组初始代码2

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -3,12 +3,46 @@ package com.nageoffer.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
  */
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+    @Override
+    public void saveGroup(String groupName) {
+
+    }
+
+    @Override
+    public void saveGroup(String username, String groupName) {
+
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        return List.of();
+    }
+
+    @Override
+    public void updateGroup(ShortLinkGroupUpdateReqDTO requestParam) {
+
+    }
+
+    @Override
+    public void deleteGroup(String gid) {
+
+    }
+
+    @Override
+    public void sortGroup(List<ShortLinkGroupSortReqDTO> requestParam) {
+
+    }
 }
