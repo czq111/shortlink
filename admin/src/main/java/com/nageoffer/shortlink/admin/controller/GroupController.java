55f9755ffa87fa0e617aec66e4364d13d37aea6d
czqq123456 <1807099124@qq.com>
2024-06-08 14:33:43 +0800

    短链接分组初始代码

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短连接分组控制层
+ */
+@RestController
+public class GroupController {
+}
