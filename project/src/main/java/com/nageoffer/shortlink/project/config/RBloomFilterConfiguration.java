8b7696690220d40dbeaf325099d97f898d32d250
czqq123456 <1807099124@qq.com>
2024-06-13 00:17:06 +0800

    新增短链接

---
+++ project/src/main/java/com/nageoffer/shortlink/project/config/RBloomFilterConfiguration.java
@@ -0,0 +1,26 @@
+package com.nageoffer.shortlink.project.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+
+
+    /**
+     * 防止短链接创建查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("shortUriCreateCachePenetrationBloomFilter");
+        //两个参数：预计加入元素大小、运行的误判率
+        cachePenetrationBloomFilter.tryInit(200000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,19 +1,79 @@
 package com.nageoffer.shortlink.project.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.project.common.convention.exception.ServiceException;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
 import com.nageoffer.shortlink.project.dao.mapper.ShortLinkMapper;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.project.service.ShortLinkService;
+import com.nageoffer.shortlink.project.util.HashUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
-        return null;
+        String shortLinkSuffix=generateSuffix(requestParam);
+        String fullShortUrl=requestParam.getDomain()+shortLinkSuffix;
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createdType(requestParam.getCreatedType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .totalPv(0)
+                .totalUv(0)
+                .totalUip(0)
+                .delTime(0L)
+                .fullShortUrl(fullShortUrl)
+       //         .favicon(getFavicon(requestParam.getOriginUrl()))
+                .build();
+        try{
+            baseMapper.insert(shortLinkDO);
+        }catch (DuplicateKeyException ex){
+            if(!shortUriCreateCachePenetrationBloomFilter.contains(shortLinkDO.getGid())){
+                shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
+            }
+            throw new ServiceException(String.format("短链接：%s 生成重复", fullShortUrl));
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
+        return ShortLinkCreateRespDTO.builder()
+                .fullShortUrl(fullShortUrl)
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .build();
+    }
+
+    private String generateSuffix(ShortLinkCreateReqDTO requestParam) {
+        int generateCount=0;
+        String shortUri;
+        while (true){
+            if(generateCount>10){
+                throw new ServiceException("短链接频繁生成，请稍后再试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            originUrl+= UUID.randomUUID().toString(); //冲突后拼上一个随机值减少冲突
+            shortUri= HashUtil.hashToBase62(originUrl);
+            //判断布隆过滤器有没有存在当前短链接
+            if(!shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain()+shortUri)){
+                break;
+            }
+            generateCount++;
+        }
+        return shortUri;
     }
 }
