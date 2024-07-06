cd4b554cc46f6fc8e28226bb79810443d608d6cd
czqq123456 <1807099124@qq.com>
2024-07-06 16:37:42 +0800

    gateway层

---
+++ gateway/src/main/java/com/nageoffer/shortlink/gateway/ShortLinkApplication.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 短链接网关
+
+ */
+@SpringBootApplication
+public class ShortLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+}
